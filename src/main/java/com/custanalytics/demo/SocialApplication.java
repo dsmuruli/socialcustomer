5b4b2e6e9b80b4c4bb3642bffa8ea35b2017d6d1
dmuruli <dmuruli@custanalytics.com>
2016-06-17 15:53:45 -0700

    Social Oauth2 successfully logging in

--- src/main/java/com/custanalytics/demo/SocialApplication.java
+++ src/main/java/com/custanalytics/demo/SocialApplication.java
@@ -1,22 +1,129 @@
 package com.custanalytics.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import java.io.IOException;
+import java.security.Principal;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
+
 @SpringBootApplication
-public class SocialApplication extends SpringBootServletInitializer {
-	private static Class<SocialApplication> applicationClass = SocialApplication.class;
+@EnableOAuth2Sso
+@RestController
+public class SocialApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialApplication.class, args);
+	}
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		System.out.println("We have just entered the user endpoint");
+		return principal;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**").authorizeRequests()
+				.antMatchers("/", "/login**", "/webjars/**").permitAll()
+				.anyRequest().authenticated().and().logout()
+				.logoutSuccessUrl("/").permitAll();
+				//.and().csrf()
+				//.csrfTokenRepository(csrfTokenRepository()).and()
+				//.addFilterAfter(csrfHeaderFilter(), CsrfFilter.class);
+	}
+
+	private Filter csrfHeaderFilter() {
+		return new OncePerRequestFilter() {
+
+			@Override
+			protected void doFilterInternal(HttpServletRequest request,
+					HttpServletResponse response, FilterChain filterChain)
+					throws ServletException, IOException {
+				CsrfToken csrf = (CsrfToken) request
+						.getAttribute(CsrfToken.class.getName());
+				if (csrf != null) {
+					Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+					String token = csrf.getToken();
+					if (cookie == null || token != null
+							&& !token.equals(cookie.getValue())) {
+						System.out.println("The facebook token is : " + token);
+						cookie = new Cookie("XSRF-TOKEN", token);
+						cookie.setPath("/");
+						response.addCookie(cookie);
+					}
+				}
+				filterChain.doFilter(request, response);
+			}
+
+		};
+	}
+
+	private CsrfTokenRepository csrfTokenRepository() {
+		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+		repository.setHeaderName("X-XSRF-TOKEN");
+		return repository;
+	}
+
+	@Autowired
+	private OAuth2ClientContext oauth2Context;
+	@Autowired
+	ResourceServerProperties facebookResource;
+
+	@Bean
+	@ConfigurationProperties("facebook.client")
+	OAuth2ProtectedResourceDetails facebook() {
+		return new AuthorizationCodeResourceDetails();
+	}
+	@Autowired
+	OAuth2RestTemplate facebookTemplate;
+	
+	/*@Bean(name="facebookTemplate")
+	OAuth2RestTemplate facebookTemplate() {
+		return new OAuth2RestTemplate(facebook(), oauth2Context);
+	}*/
+
+	/*
+	 * @Bean
+	 * 
+	 * @ConfigurationProperties("facebook.resource") ResourceServerProperties
+	 * facebookResource() { return new ResourceServerProperties(); }
+	 */
+
+	private Filter ssoFilter() {
+		OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter(
+				"/login/facebook");
+		facebookFilter.setRestTemplate(facebookTemplate);
+		facebookFilter.setTokenServices(new UserInfoTokenServices(
+				facebookResource.getUserInfoUri(), facebook().getClientId()));
+		return facebookFilter;
 	}
 
 }
